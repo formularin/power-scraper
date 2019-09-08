eb8e80987b044a057cfde995961ef052c838077a
lol-cubes <arinmkhare@gmail.com>
2019-09-08 15:06:17 -0400

    add user info process

---
+++ power_scraper/grades.py
@@ -0,0 +1,30 @@
+from os.path import abspath, dirname, isfile
+import os
+
+from selenium.webdriver import Chrome
+
+
+HOME = '/'.join(abspath(dirname(__file__)).split('/')[:3])
+
+
+def main(username, password, district):
+    """Scrapes powerschool and gives all grades"""
+
+
+if __name__ == '__main__':
+    
+    if not isfile(f'{HOME}/.user-secrets'):
+        user_secrets = []
+        user_secrets.append(input('Username: '))
+        os.system("stty -echo")
+        user_secrets.append(input('Password: '))
+        os.system("stty echo")
+        print()
+        user_secrets.append(input('District (url to sign in is powerschool.THIS.org): '))
+        with open(f'{HOME}/.user-secrets', 'w+') as f:
+            f.write('\n'.join(user_secrets))
+    
+    with open(f'{HOME}/.user-secrets', 'r') as f:
+        user_secrets = f.read().split('\n')
+
+    main(*user_secrets)
