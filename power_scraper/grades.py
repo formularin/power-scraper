437a65c443f5d65bb22e7cc5d69d29e417af6f3c
lol-cubes <arinmkhare@gmail.com>
2019-09-08 22:06:53 -0400

    complete sign-in process

--- power_scraper/grades.py
+++ power_scraper/grades.py
@@ -1,12 +1,30 @@
 import os
 from os.path import abspath, dirname, isfile
+import time
 
 from cryptography.fernet import Fernet
 from selenium.webdriver import Chrome, ChromeOptions
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
 
 
 CWD = abspath(dirname(__file__))
 HOME = '/'.join(CWD.split('/')[:3])
+
+
+def wait_for_element(driver, selector, method):
+    """Returns element after waiting for page load"""
+    try:
+        wait = WebDriverWait(driver, 10)
+        wait.until(
+            eval(f'EC.presence_of_element_located((By.{method}, "{selector}"))')
+        )
+    finally:
+        element = eval(f'driver.find_element_by_{method.lower()}("{selector}")')
+
+    return element
 
 
 def main(username, password, district):
@@ -17,6 +35,14 @@ def main(username, password, district):
     driver = Chrome(f'{CWD}/chromedriver', options=options)
 
     driver.get(f'https://powerschool.{district}.org/public/')
+
+    username_input = driver.find_element_by_id("fieldAccount")
+    username_input.send_keys(username)
+    username_input.send_keys(Keys.TAB)
+
+    password_input = driver.find_element_by_id("fieldPassword")
+    password_input.send_keys(password)
+    password_input.send_keys(Keys.ENTER)
 
 
 if __name__ == '__main__':
