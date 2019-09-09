27faa57782790cdda5d02c92df2d0ab2ddc1edae
lol-cubes <arinmkhare@gmail.com>
2019-09-08 22:36:39 -0400

    get meta info for each class

--- power_scraper/grades.py
+++ power_scraper/grades.py
@@ -1,6 +1,5 @@
 import os
 from os.path import abspath, dirname, isfile
-import time
 
 from cryptography.fernet import Fernet
 from selenium.webdriver import Chrome, ChromeOptions
@@ -30,12 +29,15 @@ def wait_for_element(driver, selector, method):
 def main(username, password, district):
     """Scrapes powerschool and gives all grades"""
 
+    classes = []  # contains dicts representing each class
+
     options = ChromeOptions()
     options.add_argument('headless')
     driver = Chrome(f'{CWD}/chromedriver', options=options)
 
     driver.get(f'https://powerschool.{district}.org/public/')
 
+    # sign in
     username_input = driver.find_element_by_id("fieldAccount")
     username_input.send_keys(username)
     username_input.send_keys(Keys.TAB)
@@ -43,6 +45,22 @@ def main(username, password, district):
     password_input = driver.find_element_by_id("fieldPassword")
     password_input.send_keys(password)
     password_input.send_keys(Keys.ENTER)
+
+    class_rows = driver.find_elements_by_xpath('//tr[@class="center"]')[:-1]
+    for row in class_rows:
+
+        class_info = {}
+
+        meta_info_td = row.find_element_by_xpath('.//td[@align="left"]')
+        meta_info = meta_info_td.text
+        email_link = meta_info_td.find_element_by_xpath('.//a[2]')
+        
+        class_info["NAME"] = meta_info.split('\n')[0][:-1]
+        class_info["ROOM"] = meta_info.split('Rm: ')[-1]
+        class_info["TEACHER"] = email_link.text[6:-1]
+        class_info["TEACHER_EMAIL"] = email_link.get_attribute('href')[7:]
+
+        classes.append(class_info)
 
 
 if __name__ == '__main__':
